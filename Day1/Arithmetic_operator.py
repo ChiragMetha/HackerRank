f026c8889a332fc2f7f6177b62fa46bcdb82805e
chirag metha <chiragmetha1805@gmail.com>
2024-07-12 13:02:42 +0530

    Day1

---
+++ Day1/Arithmetic_operator.py
@@ -0,0 +1,15 @@
+"""https://www.hackerrank.com/challenges/python-arithmetic-operators/problem"""
+
+"""The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers."""
+
+"a = 3,b = 2"
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
