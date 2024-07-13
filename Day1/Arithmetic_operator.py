b4f250ef4bb40ac6885b8bd00126ca04195c1138
chirag metha <chiragmetha1805@gmail.com>
2024-07-13 12:23:44 +0530

    Day1

--- Day1/Arithmetic_operator.py
+++ Day1/Arithmetic_operator.py
@@ -6,7 +6,7 @@ The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers."""
 
-"a = 3,b = 2 ."
+"a = 3,b = 2"
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
