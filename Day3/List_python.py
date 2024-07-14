5ff764713ad206ac70c24c4fb1c5bcd4514e8097
chirag metha <chiragmetha1805@gmail.com>
2024-07-14 18:41:15 +0530

    Day3

--- Day3/List_python.py
+++ Day3/List_python.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     N = int(input())
     List=[]
     for i in range(N):
+
         command=input().split()
         if command[0] == "insert":
             List.insert(int(command[1]),int(command[2]))
